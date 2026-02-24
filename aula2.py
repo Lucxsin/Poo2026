661956657dfc9e6359f2a30bc4ba805e8f72266b
unknown <lucashenriquesilva0809@gmail.com>
2026-02-24 09:54:37 -0300

    Entrada de dados e condicionais

---
+++ aula2.py
@@ -0,0 +1,26 @@
+#numero=int(input("Digite um numero"))
+#if(numero>100):
+    #print("A metade é",numero/2)
+#else:
+    # print("O numero não vai rodar né burro")
+
+
+#num=int(input("Digite um num "))
+#if(num % 2==0):
+   # print("Esse numero é par")
+#else:
+        #print("Não é par")
+
+num1=int(input("Digite um num"))
+num2=int(input("Digite outro num"))
+sinal=str(input("Adicione um sinal operacional"))
+
+if(sinal=="+"):
+    print("O resultado",num1+num2)
+elif(sinal =="-"):
+    print("resultado", num1-num2)
+elif(sinal=="*"):
+    print("result", num1*num2)
+elif(sinal=="/"):
+    print("resultado",num1/num2)
+    
