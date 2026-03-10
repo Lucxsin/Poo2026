8474c606250637b3bb4910b930017422ab18155f
Lucxsin <164099994+Lucxsin@users.noreply.github.com>
2026-03-10 09:34:07 -0300

    aula4.py

---
+++ aula4.py
@@ -0,0 +1,54 @@
+# ex 1
+# numeros = [0] * 10
+
+# for numero in numeros:
+#     print(numero)
+
+
+#     cont=1
+#     while cont<=20:
+#         print(cont)
+#     numero=cont+1
+
+#ex2
+# def soma_valores(valor1,valor2):
+#     calcular=valor1+valor2
+#     return calcular
+# valor1=int(input("Digite um valor"))
+# valor2=int(input("Digite um valor"))
+
+# valor=(soma_valores(valor1,valor2))
+# print(valor)
+
+# #ex 3
+# cidades=[""]*[5]
+# for cont in range(0, len(cidades)):
+#     cidades[cont]=str(input("Digite as cidades"))
+
+
+
+# for cont in range(0,len(cidades)):
+#     print(cidades[cont])
+
+#ex4 def verifica_par(numero):
+    #return numero % 2 == 0
+
+# num = int(input("Digite um número inteiro: "))
+
+# resultado = verifica_par(num)
+
+# print("O número é par?", resultado)#
+
+
+
+
+
+#ex 5
+# numero = 1
+
+# while numero != 0:
+#     numero = int(input("Digite um número inteiro (0 para sair): "))
+
+# print("Programa encerrado") 
+
+
