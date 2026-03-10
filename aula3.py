d7e779eb50fd0ea0f368f70fbcf5801043b233cd
Lucxsin <164099994+Lucxsin@users.noreply.github.com>
2026-03-10 09:33:00 -0300

    Update aula3.py

--- aula3.py
+++ aula3.py
@@ -1,1 +1,33 @@
+# for para mostrar números
+for i in range(1, 11):
+    print(i, "Im freezing outside")
 
+# criando uma lista com o mesmo número repetido
+num = int(input("Digite um numero: "))
+numeros = [num] * 10
+print(numeros)
+
+# preenchendo um vetor com valores digitados
+valores = [0] * 6
+
+for cont in range(0, len(valores)):
+    valores[cont] = int(input("Informe o valor: "))
+
+# mostrando os valores
+for cont in range(0, len(valores)):
+    print("Valor:", valores[cont])
+
+# métodos (funções)
+def fulano(a, b):
+    mult = a * b
+    print(mult)
+
+# função para ler inteiro
+def lerInt():
+    x = int(input("Informe: "))
+    return x
+
+# usando as funções
+a = lerInt()
+b = lerInt()
+fulano(a, b)
